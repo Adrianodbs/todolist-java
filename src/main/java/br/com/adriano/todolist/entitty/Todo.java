5025b300ecd7b7348d620ad2dbe5bf3cdb99193e
Adriano <adriano_alves_2008@hotmail.com>
2023-11-20 17:23:54 -0300

    Feat: create test

--- src/main/java/br/com/adriano/todolist/entitty/Todo.java
+++ src/main/java/br/com/adriano/todolist/entitty/Todo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "todos")
@@ -12,10 +13,19 @@ public class Todo {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @NotBlank
   private String name;
+  @NotBlank
   private String description;
   private Boolean done;
   private int priority;
+
+  public Todo(String name, String description, Boolean done, int priority) {
+    this.name = name;
+    this.description = description;
+    this.done = done;
+    this.priority = priority;
+  }
 
   public Long getId() {
     return id;
